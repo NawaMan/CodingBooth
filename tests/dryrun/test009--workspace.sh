ac5041dd22d4d3f3471e99574cea756a9ea26c3d
NawaMan <nawa@nawaman.net>
2026-02-06 00:26:05 -0500

    Adjust dryrun for the sandbox.

--- tests/dryrun/test009--workspace.sh
+++ tests/dryrun/test009--workspace.sh
@@ -75,6 +75,7 @@ docker \\
     -e 'BOOTH_SILENCE_BUILD=false' \\
     -e 'BOOTH_PULL=false' \\
     -e 'BOOTH_DIND=false' \\
+    -e 'BOOTH_SANDBOX=false' \\
     -e 'BOOTH_DOCKERFILE=' \\
     -e 'BOOTH_PROJECT_NAME=tests' \\
     -e 'BOOTH_TIMEZONE=America/Toronto' \\
